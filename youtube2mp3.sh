c86137e90131e1247d7a62115d2a4278aef062c6
John Regan <reganjohn@me.com>
2016-03-22 12:11:05 -0400

    Added comments

--- youtube2mp3.sh
+++ youtube2mp3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
-# A very simple Bash script to download a YouTube video 
-# and extract the music file from it. 
+# A barebones script to extract the audio track from a YouTube video 
 
 address=$1;
 regex='v=(.*)';
@@ -10,8 +9,11 @@ if [[ $address =~ $regex ]]; then
     video_id=$(echo $video_id | cut -d'&' -f1);
     video_title="$(youtube-dl --get-title $address)"; 
     youtube-dl $address;
+    # TODO: find a better way to get appropriate video file extension
     ext=$(find * -maxdepth 0 -name "*.mp4" -o -name "*.mkv" | tail -n 1 | awk -F . '{if (NF>1) {print $NF}}')
+    # convert video file to .wav format for lame
     ffmpeg -hide_banner -loglevel panic -i "$video_title-$video_id".$ext "$video_title".wav; 
+    # extract audio from .wav video file, save as .mp3 in ~/Downloads
     lame "$video_title".wav $HOME/Downloads/"$video_title".mp3;
     rm "$video_title-$video_id".$ext "$video_title".wav;
 else 
